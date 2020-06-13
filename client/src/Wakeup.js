c164943015a547fdbfe95d76e27cbf9e0bbc0e1c
ZohaibD <iamaddeveloper@gmail.com>
2020-06-13 10:41:17 +0200

    cors()

--- client/src/Wakeup.js
+++ client/src/Wakeup.js
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import { API_URL } from './config'
 export default () => 
   <div className={'loading-wrapper fadein'}>
-    <h4>My application is ruuning up, one moment please...</h4>
+    <h4>My application is ruuning up, one moment please...{API_URL}</h4>
     <div className={'loading'}>
       <div className={'background'}>
         <i className="icon-heroku"></i>
